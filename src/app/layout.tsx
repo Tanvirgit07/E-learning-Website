371fb590707b6862273c5c13bc53eea736825245
Tanvirgit07 <ahmmedtanvir9090@gamil.com>
2025-04-05 17:52:17 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import ResponsivNav from "@/Components/Home/Navebar/ResponsivNav";
+import Footer from "@/Components/Home/Footer/Footer";
+import ScrollToTop from "@/Components/Helper/ScrollToTop";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+const font = Space_Grotesk({
+  weight : ['300','400','500','600','700'],
   subsets: ["latin"],
 });
 
@@ -25,9 +29,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${font.className} antialiased`}
       >
+        <ResponsivNav />
         {children}
+        <Footer />
+        <ScrollToTop />
       </body>
     </html>
   );
